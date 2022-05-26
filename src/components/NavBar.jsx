e968fa358ad3bd5d552b64d0f0b8df1230ffb167
nicolascasa <ruizpablonicolas15@gmail.com>
2022-05-26 15:30:21 -0300

    Navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,29 @@
+import logo from '../images/logo.png'
+
+function NavBar() {
+    return( 
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#"><img src={logo}  width="30" height="30" alt=""></img> AndreStore</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Promociones</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Productos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+)}
+  export default NavBar
